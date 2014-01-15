47895101ed896bb47262b1950b0712287ba51710
Mikael Jorhult <mikael@jorhult.se>
2014-01-15 19:27:49 +0100

    Add use strict to device class.

--- src/Device.js
+++ src/Device.js
@@ -1,4 +1,6 @@
 define( [ 'midi-events' ], function( midi ) {
+	'use strict';
+	
 	function Device( input, output ) {
 		this.inputs = ( input !== undefined ? input : [] );
 		this.outputs = ( output !== undefined ? output : [] );
